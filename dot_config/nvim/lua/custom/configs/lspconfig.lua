6afbb10ab91f11cdab792595f3332ba8bfa140af
luoqi <luoqi9421@163.com>
2023-09-22 13:06:40 +0800

    add nvim config

---
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,6 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lspconfig = require("plugins.configs.lspconfig")
+local util = require "lspconfig/util"
+
+
